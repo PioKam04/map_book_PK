bbb2698ad010f7b1f34c4bccaa346aa9a4b1e9ea
PioKam04 <167193617+PioKam04@users.noreply.github.com>
2024-04-23 14:26:00 +0200

    fit: creat add was added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from models.data import users
-from utl.crud import read
+from utl.crud import read,add_user
 
 if __name__ == "__main__":
-    print(f"witaj {users[0]["name"]}")
+    print(f'witaj {users[0]["name"]}')
 
+    read(users)
+    add_user(users)
     read(users)
--- utl/crud.py
+++ utl/crud.py
@@ -1,3 +1,9 @@
 def read(users: list[dict[str, str]])->None:
     for user in users:
      print(f'twój znajomy  {user['name']} opublikował: {user['posts']} postów')
+
+def add_user(lista:list)->None:
+    user_name=input("podaj imie  ")
+    user_surname=input("podaj nazwisko ")
+    user_posts=input("podaj ile postów ")
+    lista.append({"name," : user_name, "surname": user_surname,"posts": user_posts})
