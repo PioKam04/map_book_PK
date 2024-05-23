48797ce480db4ffe5902542ab62434f4bb3511a0
PioKam04 <167193617+PioKam04@users.noreply.github.com>
2024-05-23 12:02:36 +0200

    fit: creat add was added

--- notatnik.py
+++ notatnik.py
@@ -28,3 +28,12 @@ def map_all_users(users):
 
         url=(f'https://pl.wikipedia.org/wiki/{user['location']}')
         response=requests.get(url)
+        response_html=BeautifulSoup(response.text,'html.parser')
+        longitude=float(response_html.select('.longitude')[1].text.replace(',','.'))
+        latitude=float(response_html.select('.latitude')[1].text.replace(',','.'))
+        print(longitude,latitude)
+        folium.Marker(location=[latitude, longitude], popup=f'{user['name']},postów: {user['location']},\n{user['location']}',
+                      icon=folium.Icon(color='blue')).add_to(map)
+
+    map.save(f'map.html')
+map_all_users(users)
--- utl/crud.py
+++ utl/crud.py
@@ -1,3 +1,9 @@
 def read(users: list[dict[str, str]])->None:
     for user in users:
      print(f'twój znajomy  {user['name']} opublikował: {user['posts']} postów')
+
+def search(users: list[dict[str, str]])->None:
+   user_name=input("kogo szukasz: ")
+    for user in users:
+       if user["name"] == user_name:
+        print(f'znaleziono użytkownika{user}
